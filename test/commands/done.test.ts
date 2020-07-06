68bb23cb0a77277321ebc68ae7866b7417c8e3f7
William Lindvall <shwilliam@hey.com>
2020-07-06 11:20:52 -0700

    test mark todo incomplete

--- test/commands/done.test.ts
+++ test/commands/done.test.ts
@@ -8,4 +8,13 @@ describe('done', () => {
     .it('marks complete', ctx => {
       expect(ctx.stdout).to.contain("finished 'banana'")
     })
+
+  test
+    .command(['add', 'banana'])
+    .stdout()
+    .command(['done', '0'])
+    .command(['todo', '0'])
+    .it('marks todo', ctx => {
+      expect(ctx.stdout).to.contain('still need to').and.to.contain('banana')
+    })
 })
