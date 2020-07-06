ce1dcc63c4b29370281ba547e4c3b6fb3d24f491
William Lindvall <shwilliam@hey.com>
2020-07-06 11:18:58 -0700

    test adding important todo

--- test/commands/add.test.ts
+++ test/commands/add.test.ts
@@ -5,6 +5,16 @@ describe('add', () => {
     .stdout()
     .command(['add', 'banana'])
     .it('adds todo', ctx => {
-      expect(ctx.stdout).to.contain("added 'banana'")
+      expect(ctx.stdout).to.contain('added').and.to.contain('banana')
+    })
+
+  test
+    .stdout()
+    .command(['add', 'apple!'])
+    .it('adds important todo', ctx => {
+      expect(ctx.stdout)
+        .to.contain('added')
+        .to.contain('apple')
+        .and.to.contain('important')
     })
 })
