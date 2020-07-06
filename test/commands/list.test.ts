3b07767170d8ca531b106f08be2ebe2512240611
William Lindvall <shwilliam@hey.com>
2020-07-06 11:19:37 -0700

    test list filters

--- test/commands/list.test.ts
+++ test/commands/list.test.ts
@@ -1,12 +1,47 @@
 import {expect, test} from '@oclif/test'
 
 describe('list', () => {
-  test
-    .command(['add', 'banana'])
-    .command(['add', 'apple'])
-    .stdout()
-    .command(['list'])
-    .it('lists todos', ctx => {
-      expect(ctx.stdout).to.contain('banana').and.contain('apple')
-    })
+  it('notifies of empty list', () => {
+    test
+      .stdout()
+      .command(['list'])
+      .it('lists todos', ctx => {
+        expect(ctx.stdout).to.contain('all done')
+      })
+  })
+
+  it('lists all todos', () => {
+    test
+      .command(['add', 'banana'])
+      .command(['add', 'apple'])
+      .stdout()
+      .command(['list'])
+      .it('lists todos', ctx => {
+        expect(ctx.stdout).to.contain('banana').and.contain('apple')
+      })
+  })
+
+  it('lists done todos', () => {
+    test
+      .command(['add', 'banana'])
+      .command(['add', 'apple'])
+      .command(['done', '1'])
+      .stdout()
+      .command(['list', 'done'])
+      .it('lists done todos', ctx => {
+        expect(ctx.stdout).to.contain('apple').and.not.contain('banana')
+      })
+  })
+
+  it('lists incomplete todos', () => {
+    test
+      .command(['add', 'banana'])
+      .command(['add', 'apple'])
+      .command(['done', '1'])
+      .stdout()
+      .command(['list', 'done'])
+      .it('lists done todos', ctx => {
+        expect(ctx.stdout).to.contain('banana').and.not.contain('apple')
+      })
+  })
 })
